93802b97cd3aa28b63f1b61dd515fb05719cf5a1
NathanPhilip <43943342+NathanPhilip@users.noreply.github.com>
2018-11-04 22:07:56 -0500

    Add files via upload

---
+++ EchoServer.java
@@ -0,0 +1,84 @@
+/*
+Student 1: Heyzzen Flores
+Student 2: Nathan Philip
+Course: SYST44288
+*/
+
+import java.net.*;
+import java.io.*;
+
+
+public class EchoServer {
+
+    public static void main(String args[]) {
+
+	//server and client sockets
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(3000); //port 3000
+        } 
+
+	catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();	//server will accept clients with accept method
+            } 
+	    
+	    catch (IOException e) {
+                System.out.println("Error, failed to connect");
+            	System.out.println(e.getMessage());
+            }
+
+            // Server makes new threads for additional clients
+            new NewThread(clientSocket).start();
+        }
+    }
+}
+class NewThread extends Thread {
+
+    protected Socket clientSocket;
+
+    public NewThread(Socket clientSocket) {
+
+        this.clientSocket = clientSocket;
+    }
+
+    public void run() {
+
+        InputStream in = null;
+        BufferedReader bufReaderIn = null;
+        DataOutputStream out = null;
+	String inputLine;
+
+        try {
+            in = clientSocket.getInputStream();
+            bufReaderIn = new BufferedReader(new InputStreamReader(in));
+            out = new DataOutputStream(clientSocket.getOutputStream());
+        } 
+
+	catch (IOException e) {
+            return;
+        }
+
+        while (true) {
+            try {
+                inputLine = bufReaderIn.readLine();
+                if ((inputLine == null)) {
+                    clientSocket.close();
+                    return;
+                } else {
+                    out.writeBytes(inputLine + "\n\r");
+                    out.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
+}
